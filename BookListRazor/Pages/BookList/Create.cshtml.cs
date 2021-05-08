a42bd27fe7d303569e36f927d50779453353833b
ManiaDaneshfar <maniadaneshfar@outlook.com>
2021-05-09 00:01:15 +0430

    initValidationForBook

--- BookListRazor/Pages/BookList/Create.cshtml.cs
+++ BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -10,15 +10,31 @@ namespace BookListRazor.Pages.BookList
 {
     public class CreateModel : PageModel
     {
+
         private readonly ApplicationDbContext _db;
         public CreateModel(ApplicationDbContext db)
         {
             _db = db;
         }
+        [BindProperty]
         public Book Book { get; set; }
 
         public void OnGet()
         {
+        }
+        public async Task<ActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.Book.AddAsync(Book);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
+
         }
     }
 }
