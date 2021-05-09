aee4615270db1fbedcd878ae0132c0a251323390
ManiaDaneshfar <maniadaneshfar@outlook.com>
2021-05-09 11:42:27 +0430

    initPostHaneller

--- BookListRazor/Pages/BookList/Edit.cshtml.cs
+++ BookListRazor/Pages/BookList/Edit.cshtml.cs
@@ -21,5 +21,25 @@ namespace BookListRazor.Pages.BookList
         {
             Book = await _db.Book.FindAsync(id);
         }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+               var BookFromDb= await _db.Book.FindAsync(Book.Id);
+
+                BookFromDb.Name = Book.Name;
+                BookFromDb.Author = Book.Author;
+                BookFromDb.ISBN = Book.ISBN;
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return RedirectToPage();
+
+            }
+        }
     }
 }
