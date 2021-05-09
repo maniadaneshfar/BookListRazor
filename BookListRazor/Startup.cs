f7046a413f25c03d0247929ed84e8883ad6aa965
ManiaDaneshfar <maniadaneshfar@outlook.com>
2021-05-09 13:51:23 +0430

    initGetAPI

--- BookListRazor/Startup.cs
+++ BookListRazor/Startup.cs
@@ -25,7 +25,9 @@ namespace BookListRazor
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+           
             services.AddDbContext <ApplicationDbContext>(Option => Option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllersWithViews();
             services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
@@ -52,6 +54,7 @@ namespace BookListRazor
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapRazorPages();
             });
         }
